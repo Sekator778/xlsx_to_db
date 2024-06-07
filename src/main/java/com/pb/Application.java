459fe8b1b8b83994234de99eb60b60ad2d04b662
Ніколайчук Олександр Євгенійович <oleksandr.nikolaichuk@privatbank.ua>
2024-06-07 17:17:10 +0200

    [tsk-000777] read to table dbf

--- src/main/java/com/pb/Application.java
+++ src/main/java/com/pb/Application.java
@@ -1,32 +1,51 @@
 package com.pb;
 
-
 import com.pb.datasource.FileSystemDataSource;
+import com.pb.filereader.DbfFileReader;
 import com.pb.filereader.ExcelFileReader;
+import com.pb.filereader.FileReader;
 import com.pb.service.FileProcessingService;
 import com.pb.util.TableNameUtil;
 import com.pb.writer.PostgresDatabaseWriter;
 
-import java.io.File;
+import org.apache.commons.math3.util.Pair;
 
-import static com.pb.util.TableNameUtil.createTableName;
+import java.io.File;
+import java.util.logging.Logger;
 
 public class Application {
+    private static final Logger log = Logger.getLogger(Application.class.getName());
+
     public static void main(String[] args) {
         try {
-            // Initialize the components
-            ExcelFileReader fileReader = new ExcelFileReader();
+            if (args.length != 1) {
+                log.severe("Please provide the path to the file as an argument.");
+                return;
+            }
+
+            String source = args[0];
+            File file = new File(source);
+            Pair<String, String> tableNameAndExtension = TableNameUtil.createTableNameAndExtension(file.getName());
+
+            FileReader fileReader;
+            switch (tableNameAndExtension.getSecond().toLowerCase()) {
+                case "xlsx":
+                    fileReader = new ExcelFileReader();
+                    break;
+                case "dbf":
+                    fileReader = new DbfFileReader();
+                    break;
+                default:
+                    log.severe("Unsupported file extension: " + tableNameAndExtension.getSecond());
+                    return;
+            }
+
             FileSystemDataSource fileSystemDataSource = new FileSystemDataSource();
             PostgresDatabaseWriter databaseWriter = new PostgresDatabaseWriter();
-
-            // Initialize the service with the components
             FileProcessingService fileProcessingService = new FileProcessingService(fileReader, fileSystemDataSource, databaseWriter);
 
-            // Process the file
-            String source = "files/test_data_large.xlsx";
-            File file = new File(source);
-            String tableName = createTableName(file.getName());
-            fileProcessingService.processFile(source, tableName);
+            fileProcessingService.processFile(source, file.getName());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
